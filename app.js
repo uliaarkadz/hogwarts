cec439edaae596e0adc20e9f4d2119f194fb3881
ybuiko <juliabuiko@gmail.com>
2023-12-01 18:00:57 -0500

    Commit 1 - I added my first element and it was fun

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-if (typeof jQuery == "undefined") {
-  console.log("oops! I still have to link my jQuery properly!");
-} else {
-  console.log("I did it! I linked jQuery and this js file!");
-}
+$(() => {
+  const $container = $("#container");
+  console.log($container);
+  const $h1 = $("<h1>");
+  $h1.text("Hogwarts");
+  $container.append($h1);
+  console.log($h1);
+});
