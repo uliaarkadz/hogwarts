3727cbfe97f575782fa1ac6d5d511f4f6c89e2e7
ybuiko <juliabuiko@gmail.com>
2023-12-01 21:33:31 -0500

    Commit 5 - The days go by slowly and the years go by too fast

--- app.js
+++ app.js
@@ -51,15 +51,42 @@ $(() => {
   $ul.append(
     "<li>Butter Beer</li><li class='secret'>Invisibility Cloak</li><li class='secret'>Magic Map</li><li class='secret'>Time Turner</li><li class='cat'>Leash</li><li>Bertie Bott's Every Flavor [Jelly] Beans</li>"
   );
+
   //create var for table div container
   const $classesTable = $("#classesTable");
   console.log($classesTable);
+  //create and add table and header
   const $table = $("<table>");
   $classesTable.append($table);
-  //create headear
   $table.append("<thead><th>Day</th><th>Classes</th></thead>");
 
+  //add table rows with days and classes
   classes.forEach((item) => {
     $table.append(`<tr><td>${item.day}</td><td>${item.classes}</td></tr>`);
   });
+
+  //Break your wand! (select the element that contains your wand and remove it)
+  $h4Wand.remove();
+
+  //remove just the butter beer from your list
+  $("ul li:first").remove();
+
+  //Get a new wand (add the same element back with new text describing your new wand. Be sure to insert it after your pet in the DOM)
+  $h4Wand.text("Holly Wand");
+  $h4Wand.insertAfter($h4);
+
+  //   Make your new wand stand out by adding a color of indigo (or whatever color you like). But do it with magic (jQuery): Don't add this css in your main.css file
+
+  $h4Wand.css("color", "indigo");
+
+  //   Send your pet on a spy mission (remove your pet from the DOM, put it somewhere else in your HTML). Make sure your pet's leash stays in your trunk (list item with the same class as your pet inside unordered list)
+
+  const $petDiv = $("<div class='pet'></div>");
+  $petDiv.insertAfter($classesTable);
+  $petDiv.append($h4);
+
+  //   Have your pet come back (remove your pet from the DOM, put it back in its original location)
+
+  $h4.remove();
+  $h4.insertBefore($h4Wand);
 });
